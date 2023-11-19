f287645e1d0659fa34b1b2cb5b57ad69a91b51ea
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-11-18 23:56:29 -0600

    feat(SignInForm): add dependencies and the save the value constants of the sign in form

--- components/SignInForm.jsx
+++ components/SignInForm.jsx
@@ -1,13 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function SignInForm() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //If click on the submit button, the page doesn't reload
+
+        if(!name || !email || !password) {
+            setError("All fields are necessary");
+            return;
+        }
+
+        if(name && email && password) {
+            setError("");
+            return;
+        }
+    };
+
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="shadow-xl p-5 rounded-lg border-t-4 border-indigo-400">
                 
                 <h1 className="text-xl font-bold my-4">Register</h1>
 
-                <form className="flex flex-col gap-2">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-2">
 
                     <div className="flex flex-col">
                         <label htmlFor="userName" >Full Name</label>
@@ -16,6 +39,7 @@ export default function SignInForm() {
                             placeholder="Stephen Cruise" 
                             id="userName"
                             className="inputLogin"
+                            onChange={eventInp => setName(eventInp.target.value)}
                         />
                     </div>
 
@@ -26,6 +50,7 @@ export default function SignInForm() {
                             placeholder="iamalbert@gmail.com" 
                             id="userEmail"
                             className="inputLogin"
+                            onChange={eventInp => setEmail(eventInp.target.value)}
                         />
                     </div>
 
@@ -35,15 +60,18 @@ export default function SignInForm() {
                             type="password" 
                             placeholder="********" 
                             id="userPassword"
-                            className="inputLogin" 
+                            className="inputLogin"
+                            onChange={eventInp => setPassword(eventInp.target.value)}
                         />
                     </div>
 
                     <button className="btn-primary">Register</button>
 
-                    <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-                        Error message
-                    </div>
+                    {error && (
+                        <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+                            Error message: {error}
+                        </div>
+                    )}
                     
                     <Link href={"/"} className="text-sm mt-3 text-right">
                         Already have an acount? <span className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-[length:100%_2px] bg-no-repeat bg-bottom">Log In</span>
