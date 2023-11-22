be0b90ffd86bbcbdd0076d364066c6b511d636cf
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-11-21 21:42:43 -0600

    feat(UserInfo): add functionality to Sign out button

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { signOut } from "next-auth/react"
+
 export default function UserInfo() {
     return(
         <div className="flex items-center justify-center h-screen">
@@ -9,7 +13,7 @@ export default function UserInfo() {
                     Email: <span className="font-bold text-indigo-500">john@gmail.com</span>
                 </div>
 
-                <button className="bg-indigo-400 text-white font-bold px-6 py-2 mt-3 rounded-lg transition-all duration-700 ease-in-out hover:bg-indigo-500">Log Out</button>
+                <button onClick={() => signOut()} className="bg-indigo-400 text-white font-bold px-6 py-2 mt-3 rounded-lg transition-all duration-700 ease-in-out hover:bg-indigo-500">Log Out</button>
             </div>
         </div>
     )
